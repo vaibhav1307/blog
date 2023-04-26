33117175b54c9d421bc770381e3d2776e3cd27b5
VAIBHAV <18btrct046@jainuniversity.ac.in>
2023-04-26 08:25:43 +0530

    fragmented nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Nav from './Components/Nav';
 import Blog from './Components/Blog';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Fragment>
       <Nav />
-      <Route exact path="/" component={Blog} />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Fragment>
+          <Route exact path="/" component={Blog} />
+        </Fragment>
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
